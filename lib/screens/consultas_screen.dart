f5e931171b71c2b470f5894630033218340ca5b6
fefeto <fernando.f.toniolo@gmail.com>
2022-02-06 14:42:35 -0300

    telas do ruivo

---
+++ lib/screens/consultas_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ConsultasScreen extends StatefulWidget {
+  const ConsultasScreen({ Key? key }) : super(key: key);
+
+  @override
+  _ConsultasScreenState createState() => _ConsultasScreenState();
+}
+
+class _ConsultasScreenState extends State<ConsultasScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/screens/sintomas_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SintomasScreen extends StatefulWidget {
+  const SintomasScreen({ Key? key }) : super(key: key);
+
+  @override
+  _SintomasScreenState createState() => _SintomasScreenState();
+}
+
+class _SintomasScreenState extends State<SintomasScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
